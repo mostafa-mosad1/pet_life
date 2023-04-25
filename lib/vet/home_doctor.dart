3f5ee1a05726571098349742a4808f4d0d7bed87
Mohamedabdelmonem1 <112953347+Mohamedabdelmonem1@users.noreply.github.com>
2023-04-25 20:01:33 +0200

    Add files via upload

---
+++ lib/vet/home_doctor.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class home_doctor extends StatefulWidget {
+  const home_doctor({Key? key}) : super(key: key);
+
+  @override
+  State<home_doctor> createState() => _home_doctorState();
+}
+
+class _home_doctorState extends State<home_doctor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green,);
+  }
+}
